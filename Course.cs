dcc99c71da12d22d40554035f3745ea8273b50ee
Tommy Le <82196633+Wingingbump@users.noreply.github.com>
2024-05-14 14:20:47 -0400

    added documentation

--- Course.cs
+++ Course.cs
@@ -1,19 +1,53 @@
 ﻿namespace cert_mailer
 {
+    /// <summary>
+    /// Represents a course
+    /// </summary>
     public class Course
     {
+        /// <summary>
+        /// Course Name
+        /// </summary>
         public string CourseName { get; set; }
+        /// <summary>
+        /// Prefered Course Name Between ID and Name
+        /// </summary>
         public string CourseNamingScheme { get; set; }
+        /// <summary>
+        /// Course ID
+        /// </summary>
         public string CourseId { get; set; }
+        /// <summary>
+        /// Agency Name
+        /// </summary>
         public string AgencyName { get; set; }
+        /// <summary>
+        /// Instructor for the course
+        /// NOT USED, left blank for all instances
+        /// </summary>
         public string Instructor { get; set; }
+        /// <summary>
+        /// Course Location
+        /// </summary>
         public string Location { get; set; }
+        /// <summary>
+        /// Course Start Date
+        /// </summary>
         public DateTime StartDate { get; set; }
+        /// <summary>
+        /// Course End Date
+        /// </summary>
         public DateTime EndDate { get; set; }
 
+        /// <summary>
+        /// List of all students who participated in the course.
+        /// Includes students who pass or fail, but not students who did not complete the course 
+        /// </summary>
         public List<Student> Students { get; set; }
 
-        // Default constructor
+        /// <summary>
+        /// Default Course Constructor
+        /// </summary>
         public Course()
         {
             CourseName = "";
@@ -27,6 +61,17 @@
             Students = new List<Student>();
         }
 
+        /// <summary>
+        /// Paramaterized Constructor
+        /// </summary>
+        /// <param name="courseName"> Course Name </param>
+        /// <param name="courseNamingScheme"> Prefered Course Name </param>
+        /// <param name="courseId"> Course ID </param>
+        /// <param name="agencyName"> Agency Name </param>
+        /// <param name="instructor"> Unused Instructor Name </param>
+        /// <param name="location"> Location </param>
+        /// <param name="startDate"> Start Date </param>
+        /// <param name="endDate"> End Date </param>
         public Course(string courseName, string courseNamingScheme, string courseId, string agencyName, string instructor, string location, DateTime startDate, DateTime endDate)
         {
             CourseName = courseName;
@@ -40,21 +85,38 @@
             Students = new List<Student>();
         }
 
+        /// <summary>
+        /// Adds a student to the student list
+        /// </summary>
+        /// <param name="student"> The student to add </param>
         public void AddStudent(Student student)
         {
             Students.Add(student);
         }
 
+        /// <summary>
+        /// Removes a student from the student list
+        /// </summary>
+        /// <param name="student"> The student to remove </param>
         public void RemoveStudent(Student student)
         {
             Students.Remove(student);
         }
 
+        /// <summary>
+        /// Returns a string that represents the course.
+        /// </summary>
+        /// <returns>A string that represents the course.</returns>
         public override string ToString()
         {
             return $"Course: {CourseName} ({CourseId}), Agency: {AgencyName}, Instructor: {Instructor}, Location: {Location}, Start Date: {StartDate.ToString()}, End Date: {EndDate.ToString()}, Students: {Students.Count}";
         }
 
+        /// <summary>
+        /// Equals Method
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
         public override bool Equals(object? obj)
         {
             if (obj == null || !(obj is Course))
@@ -66,6 +128,10 @@
             return CourseName == other.CourseName && CourseId == other.CourseId && AgencyName == other.AgencyName && Instructor == other.Instructor && Location == other.Location && StartDate == other.StartDate && EndDate == other.EndDate && Students.Equals(other.Students);
         }
 
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
             return HashCode.Combine(CourseName, CourseId, AgencyName, Instructor, Location, StartDate, EndDate, Students);
--- Student.cs
+++ Student.cs
@@ -1,51 +1,112 @@
-﻿namespace cert_mailer
+﻿namespace cert_mailer;
+
+/// <summary>
+/// Represents a student with information such as first name, last name, email, certification, pass status, and grade.
+/// </summary>
+public class Student
 {
-    public class Student
+    /// <summary>
+    /// Gets the first name of the student.
+    /// </summary>
+    public string FirstName
     {
-        public string FirstName { get; }
-        public string LastName { get; }
-        public string Email { get; }
-        public string Certification { get; set; }
-        public bool Pass { get; set; }
-        public string Grade
-        { get; set; }
-
-        public Student(string firstName, string lastName, string email, string certification, bool pass, string grade)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            Certification = certification;
-            Pass = pass;
-            Grade = grade;
-        }
-
-        public override string ToString()
-        {
-            return $"{FirstName} {LastName} ({Email}): {Certification} - {Pass}";
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj == null || GetType() != obj.GetType())
-            {
-                return false;
-            }
-
-            var other = (Student)obj;
-            return FirstName == other.FirstName
-                && LastName == other.LastName
-                && Email == other.Email
-                && Certification == other.Certification
-                && Pass == other.Pass;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(FirstName, LastName, Email, Certification, Pass);
-        }
-
+        get;
     }
 
+    /// <summary>
+    /// Gets the last name of the student.
+    /// </summary>
+    public string LastName
+    {
+        get;
+    }
 
+    /// <summary>
+    /// Gets the email of the student.
+    /// </summary>
+    public string Email
+    {
+        get;
+    }
+
+    /// <summary>
+    /// Gets or sets the certification of the student.
+    /// </summary>
+    public string Certification
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the student passed the certification.
+    /// </summary>
+    public bool Pass
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// Gets or sets the grade of the student.
+    /// </summary>
+    public string Grade
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Student"/> class with the specified details.
+    /// </summary>
+    /// <param name="firstName">The first name of the student.</param>
+    /// <param name="lastName">The last name of the student.</param>
+    /// <param name="email">The email of the student.</param>
+    /// <param name="certification">The certification of the student.</param>
+    /// <param name="pass">A value indicating whether the student passed the certification.</param>
+    /// <param name="grade">The grade of the student.</param>
+    public Student(string firstName, string lastName, string email, string certification, bool pass, string grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Certification = certification;
+        Pass = pass;
+        Grade = grade;
+    }
+
+    /// <summary>
+    /// Returns a string that represents the current student.
+    /// </summary>
+    /// <returns>A string that represents the current student.</returns>
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} ({Email}): {Certification} - {Pass}";
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current object.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current object.</param>
+    /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var other = (Student)obj;
+        return FirstName == other.FirstName
+            && LastName == other.LastName
+            && Email == other.Email
+            && Certification == other.Certification
+            && Pass == other.Pass;
+    }
+
+    /// <summary>
+    /// Serves as the default hash function.
+    /// </summary>
+    /// <returns>A hash code for the current object.</returns>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstName, LastName, Email, Certification, Pass);
+    }
 }
